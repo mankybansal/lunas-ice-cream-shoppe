633d3be779db4ee92363ea1d54b6230d90c83814
Mayank Bansal <sunny.bansal@gmail.com>
2024-05-23 00:58:27 -0700

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import viteCompression from "vite-plugin-compression";
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteCompression()],
   resolve: {
-    alias: [
-      { find: "~", replacement: "/src" },
-    ],
+    alias: [{ find: "~", replacement: "/src" }]
   },
   server: {
     watch: {
-      usePolling: true,
-    },
+      usePolling: true
+    }
   },
+  build: {
+    outDir: "build",
+    assetsDir: "."
+  }
 });
