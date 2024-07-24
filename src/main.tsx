3f9e2a241d46e71de876a4cc9df7e716bac87088
Mayank Bansal <sunny.bansal@gmail.com>
2024-07-24 12:25:22 -0700

    Add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "~/App";
-import { GlobalStyles } from "~/GlobalStyles.tsx";
+import { GlobalStyles } from "~/GlobalStyles";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics />
     <Global styles={GlobalStyles} />
     <App />
   </React.StrictMode>
